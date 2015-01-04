7b0fe9a8d83e31f92d1f3b36b2ceb22cff84bc00
Grzegorz Lachowski <gregory.lachowski@gmail.com>
2015-01-04 12:18:40 +0100

    impl: cached tvrage client + episodesProvider

---
+++ test/tvrage.spec.js
@@ -0,0 +1,35 @@
+describe('TvRage client', function () {
+
+  var TvRage = require('../lib/diyvod/tv/tvrage/client');
+
+  var KEY = 'test';
+  var SHOW = 'The Walking Dead';
+  var SHOW_ENCODED = 'The%20Walking%20Dead';
+
+  it ('builds request url', function () {
+
+    var client = new TvRage(KEY);
+
+    var url = client.buildRequestUrl('search', { show: SHOW });
+    expect(url).toBe('http://services.tvrage.com/myfeeds/search.php?key=' + KEY + '&show=' + SHOW_ENCODED);
+  });
+
+  it('searches for shows', function (done) {
+
+    var client = new TvRage('YDKH1Ehu25Guy1JehADu');
+    
+    client.search('Walking')
+      .then(function (data) {
+        var show = data.Results.show[0];
+        expect(show.name[0]).toBe('The Walking Dead');
+        expect(show.showid[0]).toBe('25056');
+        done();
+      }, function (reason) {
+        done(reason);
+      })
+      .catch(function (reason) {
+        done(reason);
+      });
+  }, 30000);
+
+});
