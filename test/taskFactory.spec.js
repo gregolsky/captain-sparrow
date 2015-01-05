178f02d6a72d2c4daa450a6af65888709f079d78
Grzegorz Lachowski <gregory.lachowski@gmail.com>
2015-01-05 22:12:01 +0100

    impl: tvrage episodes provider

--- test/taskFactory.spec.js
+++ test/taskFactory.spec.js
@@ -6,6 +6,19 @@ describe('Task factory', function () {
       apiKey: '',
       username: '',
     },
+    tvrage: {
+      apiKey: '',
+      shows: [],
+      cache: {
+        file: '',
+        operations: [ 'search', 'showInfo', 'episodeList' ],
+        ttl: {
+          search: 30,
+          showInfo: 30,
+          episodeList: 7
+        } 
+      },
+    },
     transmission: {
       host: 'localhost',
       port: '9091'
@@ -18,9 +31,22 @@ describe('Task factory', function () {
     }
   };
 
+  var MockFs = require('q-io/fs-mock');
+  var mockFs = MockFs({
+    "root": {
+      "a": {
+        "b": {
+          "c.txt": "Content of a/b/c.txt",
+          "fun.stuff.s01e01.mkv": 'asdg'
+        },
+        "Most.stupid.show.ever.s04e02.hdtv.mkv": 'asdg'
+      }
+    }
+  });
+
   it('resolves dependencies for tv shows download task', function () {
   
-    var task = taskFactory.resolve('tv', fakeSettings);
+    var task = taskFactory.resolve('tv', fakeSettings, mockFs);
     expect(task).toBeDefined();
     expect(task.execute).toBeDefined();
 
