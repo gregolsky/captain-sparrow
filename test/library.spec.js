c69f19d76a420b23482aac00ab9aec0fc9aa214a
Grzegorz Lachowski <gregory.lachowski@gmail.com>
2015-11-27 22:46:46 +0100

    impl: update shows lib to es6

--- test/library.spec.js
+++ test/library.spec.js
@@ -20,20 +20,23 @@ describe('TV shows library', function () {
 
   it('initializes library from directory', function(done) {
 
-    var library = new TvShowsLibrary({ tv: { libraryPath: "root/a" } }, mockFs); 
+    var library = new TvShowsLibrary({ tv: { libraryPath: "root/a" } }, mockFs);
     library.initialize()
     .then(function () {
-      should.exist(library.files.length);
-      library.files.length.should.equal(2);
-      done();
-    });;
+        should.exist(library.filenames);
+        should.exist(library.filenames.length);
+        library.filenames.length.should.equal(2);
+        done();
+    })
+    .catch(done);
+
   });
 
   it('can check if episode is already in', function(done) {
     //function Episode(show, number, season, title, airtime, runtime) {
 
     var episode = new Episode('Most stupid show  ever', '02', '04', '', null, null);
-    var library = new TvShowsLibrary({ tv: { libraryPath: "root/a" } }, mockFs); 
+    var library = new TvShowsLibrary({ tv: { libraryPath: "root/a" } }, mockFs);
 
     library.initialize()
     .then(function () {
