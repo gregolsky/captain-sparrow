345a90161ebc9ae199ad5f6358e9b503b28a5132
Grzegorz Lachowski <gregory.lachowski@gmail.com>
2016-12-16 14:25:26 +0100

    use another search provider

---
+++ test/limeTorrents.spec.js
@@ -0,0 +1,24 @@
+import LimeTorrents from 'captain-sparrow/torrentProvider/limeTorrents';
+
+describe('LimeTorrents client', function () {
+
+    /* Integration test */
+    it('finds some torrents, when asked for linux', function () {
+
+        var torrent = new LimeTorrents();
+        return torrent.search('linux')
+        .then(function (results) {
+            should.exist(results);
+            results.length.should.to.be.above(0);
+
+            var entry = results[0];
+
+            should.exist(entry);
+            should.exist(entry.name);
+            should.exist(entry.torrentLink);
+
+            (results[0].seeds >= results[1].seeds).should.be.true;
+        });
+    }, 10000);
+
+});
--- test/taskFactory.spec.js
+++ test/taskFactory.spec.js
@@ -47,7 +47,7 @@ describe('Task factory', function () {
     }));
 
     beforeEach(function () {
-        this.timeout(5000);
+        this.timeout(10000);
         taskFactory = require('captain-sparrow/taskFactory')
     });
 
